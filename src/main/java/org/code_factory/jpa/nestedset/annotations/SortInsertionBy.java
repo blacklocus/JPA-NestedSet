b6d3ca687d5294b4bdc2113df4e5020d345d2246
Justin Turner Arthur <justinarthur@gmail.com>
2012-06-15 16:48:17 -0500

    Use array of strings to specify fields to use in determining insertion point in a desired order of siblings. This is because the underlying SQL will not use `ORDER BY` clauses, but will instead use greater-than comparisons in a `WHERE` clause.

--- src/main/java/org/code_factory/jpa/nestedset/annotations/SortInsertionBy.java
+++ src/main/java/org/code_factory/jpa/nestedset/annotations/SortInsertionBy.java
@@ -15,15 +15,7 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface SortInsertionBy {
     /**
-     * An <code>orderby_list</code> similar to what is accepted by the <code>OrderBy</code> annotation.  Specified as follows:
-     *
-     * <pre>
-     *    orderby_list::= orderby_item [,orderby_item]*
-     *    orderby_item::= [property_or_field_name] [ASC | DESC]
-     * </pre>
-     *
-     * <p> If <code>ASC</code> or <code>DESC</code> is not specified,
-     * <code>ASC</code> (ascending order) is assumed.
+     * An array of entity field names that are used to determine where to insert a Node relative to its siblings.
      */
-     String value() default "";
+     String[] value() default {};
 }
